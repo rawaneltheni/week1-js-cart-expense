4f06ecf4c5928bb7f8e9933d4c7dc2dafb6358c6
rawaneltheni <rawaneltheni@gmail.com>
2025-10-13 21:37:04 +0200

    day 1 task

--- cart.js
+++ cart.js
@@ -1,3 +1,5 @@
+// Day 1 - 7 Days of JavaScript (Variables, Data Types & Arrays)
+
 let cart = [
     {name: 'Chocolate', price: 3.0, quantity: 3},
     {name: 'Tea', price: 2.0, quantity: 2},
@@ -25,3 +27,11 @@ console.table(cart);
 // to replace an item at a spesfic index
 cart.splice(3, 1, {name: 'Biscuits', price: 2.0, quantity: 5});
 console.table(cart);
+
+
+// Using template literals to log the total cost of items in the cart
+
+let cost = cart[0].price + cart[1].price + cart[2].price + cart[3].price;
+let totalItems = cart[0].quantity + cart[1].quantity + cart[2].quantity + cart[3].quantity;
+
+console.log(`The total cost of items in the cart is $${cost} for ${totalItems} items.`);
